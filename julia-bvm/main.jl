8267cc4fbe34bcc3c3894fc13c34f68c6b5588cf
Stephen <stephen@umw.edu>
2020-02-08 10:35:08 -0500

    Add graph plot animation.

--- julia-bvm/main.jl
+++ julia-bvm/main.jl
@@ -1,15 +1,36 @@
 using LightGraphs
-using GraphPlot
+using GraphPlot, Compose
+using ColorSchemes, Colors
 using Random
+using Cairo, Fontconfig
+using Glob
+
+include("agent.jl")
 
 @enum Opinion Red Blue
-function make_graph()
+function make_graph(make_anim=false)
+
+    # Set seed for consistency.
+    Random.seed!(12345)
+
+    # Put us in a temp dir, and remove any old files from previous runs.
+    save_dir = pwd()
+    cd("$(tempdir())")
+    rm.(glob("graph*.png"))
+    rm.(glob("graph*.svg"))
+
     n = 20
     p = 0.2
     graph = erdos_renyi(n,p)
     while is_connected(graph) == false
         graph = erdos_renyi(n,p)
     end
+
+    # Plot initial graph layout, and remember coordinates.
+    if make_anim
+        locs_x, locs_y = spring_layout(graph)
+    end
+
     node_list = vertices(graph)
     agent_list = []
     opin_list = (Red::Opinion, Blue::Opinion)
@@ -17,8 +38,38 @@ function make_graph()
         this_opinion = rand(opin_list)
         push!(agent_list, Agent(this_opinion))
     end
+
     uniform = false
+    iter = 1
+    println("Iterations:")
+
     while uniform == false
+
+        if iter % 40 > 0
+            print(".")
+        else
+            println(iter)
+        end
+
+        if make_anim
+            # Remember and reuse graph layout for each animation frame.
+            remember_layout = x -> spring_layout(x, locs_x, locs_y)
+
+            # Plot this frame of animation to a file.
+            graphp = gplot(graph,
+                layout=remember_layout,
+                NODESIZE=.08,
+                nodestrokec=colorant"grey",
+                nodestrokelw=.5,
+                nodefillc=[ ifelse(a.opinion==Blue::Opinion,colorant"blue",
+                    colorant"red") for a in agent_list ])
+            draw(PNG("$(tempdir())/graph$(lpad(string(iter),3,'0')).png"),
+                graphp)
+            run(`mogrify -format svg -gravity South -pointsize 15 -annotate 0 
+                "Iteration $(iter) "
+                $(joinpath(tempdir(),"graph"))$(lpad(string(iter),3,'0')).png`)
+        end
+
         uniform = true
         for i in 1:n-1
             if getOpinion(agent_list[i]) != getOpinion(agent_list[i+1])
@@ -33,6 +84,16 @@ function make_graph()
         next_agent = agent_list[next_node]
         next_opinion = getOpinion(next_agent)
         setOpinion(this_agent, next_opinion)
+        iter += 1
     end
-    gplot(graph)
+
+    println(iter)
+    if make_anim
+        println("Building animation...")
+        run(`convert -delay 15 graph*.svg graph.gif`)
+        println("...animation in $(tempdir())/graph.gif.")
+    end
+
+    # Return to user's original directory.
+    cd(save_dir)
 end
