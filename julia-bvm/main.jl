cc20a95a994ec6b180baa3685bcfad3aa8a8e95a
hannahfrederickcpsc <hannahfrederick@outlook.com>
2020-02-17 17:47:23 -0500

    made dataframe and some plots (implemented param sweep)

--- julia-bvm/main.jl
+++ julia-bvm/main.jl
@@ -5,93 +5,29 @@ using Random
 using Cairo, Fontconfig
 using Glob
 using Plots
+using DataFrames
 
 include("agent.jl")
 include("config.jl")
 @enum Opinion Red Blue
-function run_sim(make_anim=false, influencer=false, replacement=false)
+function make_graph(n=20, p=0.2, make_anim=false, influencer=false, replacement=false)
 
     # Set seed for consistency.
-    Random.seed!(12345)
+    #Random.seed!(12345)
 
     # Put us in a temp dir, and remove any old files from previous runs.
-    save_dir = pwd()
+    #save_dir = pwd()
     cd("$(store_dir)")
     rm.(glob("graph*.png"))
     rm.(glob("graph*.svg"))
 
-    n = 20
-    p = 0.2
     graph = erdos_renyi(n,p)
     while is_connected(graph) == false
         graph = erdos_renyi(n,p)
     end
 
-    node_list = Array(vertices(graph))
-    agent_list = []
-    percent_red_list = []
-    opin_list = (Red::Opinion, Blue::Opinion)
-    for n in node_list
-        this_opinion = rand(opin_list)
-        push!(agent_list, Agent(this_opinion))
-    end
+    return graph
 
-    uniform = false
-    iter = 1
-    println("Iterations:")
-    use_node_list = copy(node_list)
-    use_agent_list = copy(agent_list)
-
-    while uniform == false
-        num_red = find_num_red(agent_list)
-        percent_red = num_red/n
-        push!(percent_red_list, percent_red)
-
-        if iter % 40 > 0
-            print(".")
-        else
-            println(iter)
-        end
-
-        if make_anim
-            make_graph_anim(graph, agent_list, iter)
-        end
-
-        uniform = true
-        for i in 1:n-1
-            if getOpinion(agent_list[i]) != getOpinion(agent_list[i+1])
-                uniform = false
-                break
-            end
-        end
-
-        if influencer == false
-            if replacement == false && length(use_node_list) == 0
-                    use_node_list = copy(node_list)
-                    use_agent_list = copy(agent_list)
-            end
-            set_influencer_opinion(graph, use_node_list, use_agent_list, replacement)
-        else
-            if replacement == false && length(use_node_list) == 0
-                    use_node_list = copy(node_list)
-                    use_agent_list = copy(agent_list)
-            end
-            set_influencee_opinion(graph, use_node_list, use_agent_list, replacement)
-        end
-        iter += 1
-    end
-
-    println(iter)
-    display(plot(1:length(percent_red_list),percent_red_list, title="percent red opinion for each iteration", xlabel="number of iterations",ylabel="percent red opinion",seriescolor = :red))
-    savefig("per_red_plot.png")
-    if make_anim
-        println("Building animation...")
-        run(`convert -delay 15 graph*.svg graph.gif`)
-        println("...animation in $(tempdir())/graph.gif.")
-    end
-
-    # Return to user's original directory.
-    cd(save_dir)
 end
 
 function set_influencee_opinion(this_graph, this_node_list, this_agent_list, replacement)
@@ -157,4 +93,101 @@ function make_graph_anim(this_graph, this_agent_list, this_iter)
     run(`mogrify -format svg -gravity South -pointsize 15 -annotate 0
         "Iteration $(iter) "
         "$(store_dir)/graph"$(lpad(string(iter),3,'0')).png`)
+end
+
+function run_sim(n=20, p=0.2, make_anim=false, influencer=false, replacement=false)
+    save_dir = pwd()
+    graph = make_graph(n, p, make_anim, influencer, replacement)
+    node_list = Array(vertices(graph))
+    n = nv(graph)
+    agent_list = []
+    opin_list = (Red::Opinion, Blue::Opinion)
+    for n in node_list
+        this_opinion = rand(opin_list)
+        push!(agent_list, Agent(this_opinion))
+    end
+    uniform = false
+    iter = 1
+    #println("Iterations:")
+    percent_red_list = []
+    use_node_list = copy(node_list)
+    use_agent_list = copy(agent_list)
+
+    while uniform == false
+        num_red = find_num_red(agent_list)
+        percent_red = num_red/n
+        push!(percent_red_list, percent_red)
+
+        if iter % 40 > 0
+            #print(".")
+        else
+            #println(iter)
+        end
+
+        if make_anim
+            make_graph_anim(graph, agent_list, iter)
+        end
+
+        uniform = true
+        for i in 1:n-1
+            if getOpinion(agent_list[i]) != getOpinion(agent_list[i+1])
+                uniform = false
+                break
+            end
+        end
+
+        if influencer == false
+            if replacement == false && length(use_node_list) == 0
+                    use_node_list = copy(node_list)
+                    use_agent_list = copy(agent_list)
+            end
+            set_influencer_opinion(graph, use_node_list, use_agent_list, replacement)
+        else
+            if replacement == false && length(use_node_list) == 0
+                    use_node_list = copy(node_list)
+                    use_agent_list = copy(agent_list)
+            end
+            set_influencee_opinion(graph, use_node_list, use_agent_list, replacement)
+        end
+        iter += 1
+    end
+
+    #println(iter)
+    #display(plot(1:length(percent_red_list),percent_red_list, title="percent red opinion for each iteration", xlabel="number of iterations",ylabel="percent red opinion",seriescolor = :red))
+    #savefig("per_red_plot.png")
+    if make_anim
+        #println("Building animation...")
+        run(`convert -delay 15 graph*.svg graph.gif`)
+        #println("...animation in $(tempdir())/graph.gif.")
+    end
+
+    # Return to user's original directory.
+    cd(save_dir)
+    return iter
+end
+
+function param_sweep(num_runs=10, make_anim=false, influencer=false, replacement=false)
+    n_list = []
+    p_list = []
+    avg_steps_list = []
+    for n in 10:10:100
+        for p in 0.1:0.1:1.0
+            push!(n_list, n)
+            push!(p_list, p)
+            count_steps = 0
+            for x in 1:num_runs
+                count_steps += run_sim(n, p, make_anim, influencer, replacement)
+            end
+            avg_steps = count_steps/num_runs
+            push!(avg_steps_list, avg_steps)
+
+        end
+
+    end
+    df = DataFrame(N = n_list, P = p_list, STEPS = avg_steps_list)
+    showall(df)
+    display(plot(n_list, avg_steps_list, title= "number of nodes vs average number of steps", xlabel="number of nodes", ylabel="number of steps"))
+    savefig("n_list_plot.png")
+    display(plot(p_list, avg_steps_list, title= "probability of neighbor vs average number of steps", xlabel="probability of neighbor", ylabel="number of steps"))
+    savefig("p_list_plot.png")
 end
