1253e2b1828d86db9501f139f6b26b7b2b5afee3
Simeon Neisler <simeon.neisler@gmail.com>
2020-02-20 15:04:01 -0500

    added example file of config.jl

---
+++ julia-bvm/config-example.jl
@@ -0,0 +1,8 @@
+#This is an example of a "config.jl" file. It stores all of the config information to make this run on your system
+
+#export the store_dir variable below
+#export store_dir
+
+#store_dir is the directory you want your program to store the results (graphs, images, etc) of running this program
+
+#store_dir="/directory/you/want/to/store/in"
